3fea8a32c1eacb0b0536da029d79505bc7fd50eb
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-07-21 10:25:16 -0400

    Bonus exercises done

--- BL-20-Introducao-SQL/20.3/script.sql
+++ BL-20-Introducao-SQL/20.3/script.sql
@@ -20,3 +20,31 @@ SELECT DISTINCT Fornecedor FROM PecasFornecedores.Fornecimentos WHERE Preco >= 1
 
 -- Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019.
 SELECT COUNT(*) FROM PecasFornecedores.Vendas WHERE DATE(order_date) BETWEEN '2018-04-15' AND '2019-07-30'; 
+
+-- BONUS --
+
+-- Using Scientists Schema
+
+-- Escreva uma query para exibir todas as informações de todos os cientistas que possuam a letra e em seu nome.
+SELECT * FROM Scientists.Scientists WHERE Name LIKE '%e%';
+
+-- Escreva uma query para exibir o nome de todos os projetos cujo o código inicie com a letra A . Ordene o resultado em ordem alfabética.
+SELECT Name FROM Scientists.Projects WHERE Code LIKE 'A%' ORDER BY Name;
+
+-- Escreva uma query para exibir o código e nome de todos os projetos que possuam em seu código o número 3 . Ordene o resultado em ordem alfabética.
+SELECT Code, Name FROM Scientists.Projects WHERE Code LIKE '%3%' ORDER BY Name;
+
+-- Escreva uma query para exibir todos os cientistas (valores numéricos) cujos projetos sejam AeH3 , Ast3 ou Che1.
+SELECT COUNT(*) FROM Scientists.AssignedTo WHERE Project IN('AeH3', 'Ast3', 'Che1');
+
+-- Escreva uma query para exibir todas as informações de todos os projetos com mais de 500 horas.
+SELECT * FROM Scientists.Projects WHERE Hours > 500;
+
+-- Escreva uma query para exibir todas as informações de todos os projetos cujas horas sejam maiores que 250 e menores 800.
+SELECT * FROM Scientists.Projects WHERE Hours BETWEEN 251 AND 799;
+
+-- Escreva uma query para exibir o nome e o código de todos os projetos cujo nome NÃO inicie com a letra A.
+SELECT Name, Code FROM Scientists.Projects WHERE Name NOT LIKE 'A%';
+
+-- Escreva uma query para exibir o nome de todos os projetos cujo código contenha a letra H.
+SELECT Name FROM Scientists.Projects WHERE Code LIKE '%H%';
